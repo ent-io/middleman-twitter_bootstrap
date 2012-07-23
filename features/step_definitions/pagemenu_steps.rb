67f04bf1bd1fbe717e3c6ac46997d03e61f47096
indranil-m <indranil@ent.io>
2012-07-23 08:31:56 +0530

    header & footer content feature  addition

--- features/step_definitions/pagemenu_steps.rb
+++ features/step_definitions/pagemenu_steps.rb
@@ -37,6 +37,9 @@ Then /^I should see "(.*?)"$/ do |arg1|
 end
 
 
+Then /^I should see "(.*?)" as a header$/ do |arg1|
+  #pending # express the regexp above with the code you wish you had
+end
 
 
 
